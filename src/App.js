16d5fb0eaff54900266ced2e102b4e5817ee8a76
Oscar Angel Cardenas Aguilar <oscar.cardenas@oscar.cardenas>
2021-04-24 19:51:42 -0500

    Start cliente

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ClienteIndex from './components/cliente/index'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <ClienteIndex></ClienteIndex>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
