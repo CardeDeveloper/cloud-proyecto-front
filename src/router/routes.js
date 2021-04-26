1beed2de8b0ac4d8b748c05499c6534d42e2593b
AlejandroGodinez <is714181@iteso.mx>
2021-04-26 08:16:40 -0500

    update edit user

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route} from "react-router-dom";
 import Login from '../containers/Login/login';
+import EditUser from '../containers/editUser/editUser';
 
 const Routes = () =>{
     return(
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' render={()=> <Login/>}/>
+                <Route exact path='/edit' render={()=> <EditUser/>}/>
                 {/* <Route component={Login}/> */}
             </Switch>
         </BrowserRouter>
