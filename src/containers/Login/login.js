fff63df169ae28be71a395906ccd587865599951
AlejandroGodinez <is714181@iteso.mx>
2021-04-25 22:59:13 -0500

    finished login page

--- src/containers/Login/login.js
+++ src/containers/Login/login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import '../../styles/components/login.scss';
 
 const Login = () =>{
     return(
-        <div>
-            <h1>Hola</h1>
+        <div className='login-body'>
+            <div className='login-container'>
+                <form >
+                    <h1>Inicio de Sesión</h1>
+                    <label htmlFor="fname">Correo</label>
+                    <input type="text" id="fname" className="correo" placeholder="Ingrese su correo..."/>
+                    <label htmlFor="fname">Password</label>
+                    <input type="password" id="fname" className="passwrd" placeholder="Contraseña..."/>
+                    <Button variant="secondary">Olvidé mi contraseña</Button>
+                    <Button variant="primary" type="submit">Iniciar Sesión</Button>
+                </form>
+            </div>
         </div>
     )
 }
