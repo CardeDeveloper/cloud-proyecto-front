4207d2b702f29776bd063259dc89d26e4fc74d4b
Oscar Cardenas <oscarangel_cardenas@hotmail.com>
2021-04-26 08:28:38 -0500

    routes added

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route} from "react-router-dom";
 import Login from '../containers/Login/login';
+import ClienteIndex from '../containers/Cliente/index';
 
 const Routes = () =>{
     return(
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' render={()=> <Login/>}/>
+                
                 {/* <Route component={Login}/> */}
+                <Route exact path='/cliente' render={()=> <ClienteIndex/>}/>
             </Switch>
         </BrowserRouter>
     )
