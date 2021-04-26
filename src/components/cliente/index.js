2f3dbd3e37445137a9325bd0c2e85c6d1900241e
Oscar Angel Cardenas Aguilar <oscar.cardenas@oscar.cardenas>
2021-04-26 07:39:43 -0500

    incio cliente

--- src/components/cliente/index.js
+++ src/components/cliente/index.js
@@ -1,23 +1,84 @@
-import {Navbar,Nav, CardDeck,Card,Button} from 'react-bootstrap'
+import {Navbar,Nav, CardDeck,Card,Button,Col,Row} from 'react-bootstrap'
 import Menu from '../menu'
 function ClienteIndex() {
     return (
         <section>
             <Menu></Menu>
-            <Button></Button>
-            <CardDeck>
-                <Card className="text-center">
-                    <Card.Header>Featured</Card.Header>
+            <Button className="mb-3">Agregar</Button>
+            <Row>
+                <Col md={3}>
+                <Card  className="text-center mb-2">
+                    <Card.Header>#ID</Card.Header>
                     <Card.Body>
-                        <Card.Title>Special title treatment</Card.Title>
+                        <Card.Title>Oscar Cardenas</Card.Title>
                         <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
+                        <p>oscar@mail.com</p>
+                        <p>3333333333</p>
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="warning">Editar</Button>
+                        <Button variant="danger">Eliminar</Button>
                     </Card.Body>
-                    <Card.Footer className="text-muted">2 days ago</Card.Footer>
+                    <Card.Footer className="text-muted">Agregado hace 2 dias</Card.Footer>
                 </Card>
-            </CardDeck>
+                </Col>
+                <Col md={3}>
+                <Card  className="text-center mb-2">
+                    <Card.Header>#ID</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Oscar Cardenas</Card.Title>
+                        <Card.Text>
+                        <p>oscar@mail.com</p>
+                        <p>3333333333</p>
+                        </Card.Text>
+                        <Button variant="warning">Editar</Button>
+                        <Button variant="danger">Eliminar</Button>
+                    </Card.Body>
+                    <Card.Footer className="text-muted">Agregado hace 2 dias</Card.Footer>
+                </Card>
+                </Col><Col md={3}>
+                <Card  className="text-center mb-2">
+                    <Card.Header>#ID</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Oscar Cardenas</Card.Title>
+                        <Card.Text>
+                        <p>oscar@mail.com</p>
+                        <p>3333333333</p>
+                        </Card.Text>
+                        <Button variant="warning">Editar</Button>
+                        <Button variant="danger">Eliminar</Button>
+                    </Card.Body>
+                    <Card.Footer className="text-muted">Agregado hace 2 dias</Card.Footer>
+                </Card>
+                </Col><Col md={3}>
+                <Card  className="text-center mb-2">
+                    <Card.Header>#ID</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Oscar Cardenas</Card.Title>
+                        <Card.Text>
+                        <p>oscar@mail.com</p>
+                        <p>3333333333</p>
+                        </Card.Text>
+                        <Button variant="warning">Editar</Button>
+                        <Button variant="danger">Eliminar</Button>
+                    </Card.Body>
+                    <Card.Footer className="text-muted">Agregado hace 2 dias</Card.Footer>
+                </Card>
+                </Col><Col md={3}>
+                <Card  className="text-center mb-2">
+                    <Card.Header>#ID</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Oscar Cardenas</Card.Title>
+                        <Card.Text>
+                        <p>oscar@mail.com</p>
+                        <p>3333333333</p>
+                        </Card.Text>
+                        <Button variant="warning">Editar</Button>
+                        <Button variant="danger">Eliminar</Button>
+                    </Card.Body>
+                    <Card.Footer className="text-muted">Agregado hace 2 dias</Card.Footer>
+                </Card>
+                </Col>
+            </Row>
         </section>
     );
   }
