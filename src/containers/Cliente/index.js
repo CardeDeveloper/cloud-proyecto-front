e2a13458bf4ac83fcc775766622e60636312d62c
AlejandroGodinez <is714181@iteso.mx>
2021-04-28 20:44:53 -0500

    created modal on usesr edition

--- src/containers/Cliente/index.js
+++ src/containers/Cliente/index.js
@@ -1,6 +1,11 @@
-import {Navbar,Nav, CardDeck,Card,Button,Col,Row} from 'react-bootstrap'
+import {Navbar,Nav, CardDeck,Card,Button,Col,Row,Modal,Form} from 'react-bootstrap'
+import React, { useState } from 'react';
 import Menu from '../menu'
 function ClienteIndex() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <section>
             <Menu></Menu>
@@ -15,7 +20,7 @@ function ClienteIndex() {
                         <p>oscar@mail.com</p>
                         <p>3333333333</p>
                         </Card.Text>
-                        <Button variant="warning">Editar</Button>
+                        <Button variant="warning" onClick={handleShow}>Editar</Button>
                         <Button variant="danger">Eliminar</Button>
                     </Card.Body>
                     <Card.Footer className="text-muted">Agregado hace 2 dias</Card.Footer>
@@ -79,6 +84,29 @@ function ClienteIndex() {
                 </Card>
                 </Col>
             </Row>
+            <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+            <Modal.Title>Edición del Usuario</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form.Group >
+                    <Form.Label>Nombre: </Form.Label>
+                    <Form.Control type="text" placeholder="Nombre del Usuario"/>           
+                    <Form.Label>Correo: </Form.Label>
+                    <Form.Control type="text" placeholder="Correo del Usuario"/>           
+                    <Form.Label>Teléfono: </Form.Label>
+                    <Form.Control type="text" placeholder="Teléfono del Usuario"/>           
+                </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+                Cerrar
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+                Guardar
+            </Button>
+            </Modal.Footer>
+        </Modal>
         </section>
     );
   }
