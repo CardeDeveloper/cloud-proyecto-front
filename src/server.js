bd91147e2fa4d253869da207be75bf6ad4180e60
Oscar Cardenas <oscarangel_cardenas@hotmail.com>
2021-05-10 09:01:50 -0500

    changes2

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const http = require("http")
 const path = require("path")
 const app = express()
 
-app.use(express.static(path.join(__dirname, "build")))
+app.use(express.static(path.join(__dirname, "/build")))
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "build", "index.html"))
+	res.sendFile(path.join(__dirname, "/build/index.html"))
 })
 
 const server = http.createServer(app)
